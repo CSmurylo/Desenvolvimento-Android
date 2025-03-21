7f3192b06ce1fcbd428212e5b50187ac8114f41c
Murylo Costa Santos <166061941+CSmurylo@users.noreply.github.com>
2025-03-20 21:05:41 -0300

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,34 @@
+const styles = {
+  container: {
+    backgroundColor: "#f5f5f5",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  overlay: {
+    backgroundColor: "#00000006",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  content: {
+    width: 250,
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 15,
+    fontWeight: "bold",
+    padding: "15 0"
+  },
+  message: {
+    fontSize: 16,
+    marginBottom: 15,
+    color: "#555"
+  }
+}
+
+export default styles;
